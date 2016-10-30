6cd3d0d23489ced43bd7576117891fb2861e6d47
James Bove <jebove@ucsc.edu>
2016-10-30 11:42:46 -0700

    added leftovers from hw2 (and started server)

--- web2py/applications/web2py_start_2016/controllers/default.py
+++ web2py/applications/web2py_start_2016/controllers/default.py
@@ -22,7 +22,21 @@ def index():
     """
     This is your main controller.  Here you do almost nothing; you just cause index.html to be served.
     """
-    return dict()
+    """
+    Code from last assignment. Might come in handy? If you see this commented out, grader, sorry, I meant to use or delete it.
+    post_list = []
+    i = 0
+    for row in db().select(db.post.ALL, orderby=~db.post.created_on):
+        post_list.append(row)
+        i += 1
+        if i >= 20:
+            break
+    if not post_list:
+        return dict(posts='none')
+    else:
+        return dict(posts=post_list, get_user_name_from_email = get_user_name_from_email)
+    """
+    return dict(get_user_name_from_email=get_user_name_from_email)
 
 
 @auth.requires_login()
@@ -30,7 +44,21 @@ def edit():
     """
     This is the page to create / edit / delete a post.
     """
-    return dict()
+    if not request.args:
+        form=SQLFORM(db.post)
+    else:
+        post = db.post[long(request.args[0])]
+        if post is None:
+            return dict(page='No post by id ' + str(request.args[0]) + ' exists.')
+        elif not auth.user_id or auth.user.email != post.user_email:
+            return dict(page='You are not allowed to edit post ' + str(post.id) + ' because it belongs to another user.')
+        else:
+            form=SQLFORM(db.post, post.id, deletable=True)
+            form.add_button('Cancel', URL('default', 'index'))
+    if form.process().accepted:
+        session.flash = T('Post added/edited.')
+        redirect(URL('default', 'index'))
+    return dict(page=form)
 
 
 def user():
