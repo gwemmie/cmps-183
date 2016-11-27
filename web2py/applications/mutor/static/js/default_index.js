e3a89124cecad633399671186925af933b4c4c34
James Bove <jebove@ucsc.edu>
2016-11-27 15:16:57 -0800

    added accidentals and key support, among other things

--- web2py/applications/mutor/static/js/default_index.js
+++ web2py/applications/mutor/static/js/default_index.js
@@ -12,179 +12,252 @@ var app = function() {
     };
 
     VF = Vex.Flow;
-    var i = 0;
-
-    // Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById("boo");
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
-    // Configure the rendering context.
-    renderer.resize(500, 500);
-    var context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    var stave = new VF.Stave(10, 40, 400);
-
-    // Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
-
-    // Add notes
-    var notes = [
-        // A quarter-note C.
-        new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
-
-        // A quarter-note D.
-        new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
-
-        // A quarter-note rest. Note that the key (b/4) specifies the vertical
-        // position of the rest.
-        new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
-
-        // A C-Major chord.
-        new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
-    ];
-
-    var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
-    voice.addTickables(notes);
-
-    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
-
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-
-    voice.draw(context, stave);
 
     //maps vextab notes to midi numbers using associative array
-    var vtm = new Object(); // or just {}
-    vtm['c/0'] = 0;
-    vtm['c#/0'] = 1;
-    vtm['d/0'] = 2;
-    vtm['d#/0'] = 3;
-    vtm['e/0'] = 4;
-    vtm['f/0'] = 5;
-    vtm['f#/0'] = 6;
-    vtm['g/0'] = 7;
-    vtm['g#/0'] = 8;
-    vtm['a/0'] = 9;
-    vtm['a#/0'] = 10;
-    vtm['b/0'] = 11;
-    vtm['c/1'] = 12;
-    vtm['c#/1'] = 13;
-    vtm['d/1'] = 14;
-    vtm['d#/1'] = 15;
-    vtm['e/1'] = 16;
-    vtm['f/1'] = 17;
-    vtm['f#/1'] = 18;
-    vtm['g/1'] = 19;
-    vtm['g#/1'] = 20;
-    vtm['a/1'] = 21;
-    vtm['a#/1'] = 22;
-    vtm['b/1'] = 23;
-    vtm['c/2'] = 24;
-    vtm['c#/2'] = 25;
-    vtm['d/2'] = 26;
-    vtm['d#/2'] = 27;
-    vtm['e/2'] = 28;
-    vtm['f/2'] = 29;
-    vtm['f#/2'] = 30;
-    vtm['g/2'] = 31;
-    vtm['g#/2'] = 32;
-    vtm['a/2'] = 33;
-    vtm['a#/2'] = 34;
-    vtm['b/2'] = 35;
-    vtm['c/3'] = 36;
-    vtm['c#/3'] = 37;
-    vtm['d/3'] = 38;
-    vtm['d#/3'] = 39;
-    vtm['e/3'] = 40;
-    vtm['f/3'] = 41;
-    vtm['f#/3'] = 42;
-    vtm['g/3'] = 43;
-    vtm['g#/3'] = 44;
-    vtm['a/3'] = 45;
-    vtm['a#/3'] = 46;
-    vtm['b/3'] = 47;
-    vtm['c/4'] = 48;
-    vtm['c#/4'] = 49;
-    vtm['d/4'] = 50;
-    vtm['d#/4'] = 51;
-    vtm['e/4'] = 52;
-    vtm['f/4'] = 53;
-    vtm['f#/4'] = 54;
-    vtm['g/4'] = 55;
-    vtm['g#/4'] = 56;
-    vtm['a/4'] = 57;
-    vtm['a#/4'] = 58;
-    vtm['b/4'] = 59;
-    vtm['c/5'] = 60;
-    vtm['c#/5'] = 61;
-    vtm['d/5'] = 62;
-    vtm['d#/5'] = 63;
-    vtm['e/5'] = 64;
-    vtm['f/5'] = 65;
-    vtm['f#/5'] = 66;
-    vtm['g/5'] = 67;
-    vtm['g#/5'] = 68;
-    vtm['a/5'] = 69;
-    vtm['a#/5'] = 70;
-    vtm['b/5'] = 71;
-    vtm['c/6'] = 72;
-    vtm['c#/6'] = 73;
-    vtm['d/6'] = 74;
-    vtm['d#/6'] = 75;
-    vtm['e/6'] = 76;
-    vtm['f/6'] = 77;
-    vtm['f#/6'] = 78;
-    vtm['g/6'] = 79;
-    vtm['g#/6'] = 80;
-    vtm['a/6'] = 81;
-    vtm['a#/6'] = 82;
-    vtm['b/6'] = 83;
-    vtm['c/7'] = 84;
-    vtm['c#/7'] = 85;
-    vtm['d/7'] = 86;
-    vtm['d#/7'] = 87;
-    vtm['e/7'] = 88;
-    vtm['f/7'] = 89;
-    vtm['f#/7'] = 90;
-    vtm['g/7'] = 91;
-    vtm['g#/7'] = 92;
-    vtm['a/7'] = 93;
-    vtm['a#/7'] = 94;
-    vtm['b/7'] = 95;
-    vtm['c/8'] = 96;
-    vtm['c#/8'] = 97;
-    vtm['d/8'] = 98;
-    vtm['d#/8'] = 99;
-    vtm['e/8'] = 100;
-    vtm['f/8'] = 101;
-    vtm['f#/8'] = 102;
-    vtm['g/8'] = 103;
-    vtm['g#/8'] = 104;
-    vtm['a/8'] = 105;
-    vtm['a#/8'] = 106;
-    vtm['b/8'] = 107;
-    vtm['c/9'] = 108;
-    vtm['c#/9'] = 109;
-    vtm['d/9'] = 110;
-    vtm['d#/9'] = 111;
-    vtm['e/9'] = 112;
-    vtm['f/9'] = 113;
-    vtm['f#/9'] = 114;
-    vtm['g/9'] = 115;
-    vtm['g#/9'] = 116;
-    vtm['a/9'] = 117;
-    vtm['a#/9'] = 118;
-    vtm['b/9'] = 119;
-    vtm['c/10'] = 120;
-    vtm['c#/10'] = 121;
-    vtm['d/10'] = 122;
-    vtm['d#/10'] = 123;
-    vtm['e/10'] = 124;
-    vtm['f/10'] = 125;
-    vtm['f#/10'] = 126;
-    vtm['g/10'] = 127;
+    var vtm = {
+	'c/0': 0,
+	'c#/0': 1,
+	'db/0': 1,
+	'd/0': 2,
+	'd#/0': 3,
+	'eb/0': 3,
+	'e/0': 4,
+	'e#/0': 5,
+	'fb/0': 4,
+	'f/0': 5,
+	'f#/0': 6,
+	'gb/0': 6,
+	'g/0': 7,
+	'g#/0': 8,
+	'ab/0': 8,
+	'a/0': 9,
+	'a#/0': 10,
+	'bb/0': 10,
+	'b/0': 11,
+	'b#/0': 12,
+	'cb/1': 11,
+	'c/1': 12,
+	'c#/1': 13,
+	'db/1': 13,
+	'd/1': 14,
+	'd#/1': 15,
+	'eb/1': 15,
+	'e/1': 16,
+	'e#/1': 17,
+	'fb/1': 16,
+	'f/1': 17,
+	'f#/1': 18,
+	'gb/1': 18,
+	'g/1': 19,
+	'g#/1': 20,
+	'ab/1': 20,
+	'a/1': 21,
+	'a#/1': 22,
+	'bb/1': 22,
+	'b/1': 23,
+	'b#/1': 24,
+	'cb/2': 23,
+	'c/2': 24,
+	'c#/2': 25,
+	'db/2': 25,
+	'd/2': 26,
+	'd#/2': 27,
+	'eb/2': 27,
+	'e/2': 28,
+	'e#/2': 29,
+	'fb/2': 28,
+	'f/2': 29,
+	'f#/2': 30,
+	'gb/2': 30,
+	'g/2': 31,
+	'g#/2': 32,
+	'ab/2': 32,
+	'a/2': 33,
+	'a#/2': 34,
+	'bb/2': 34,
+	'b/2': 35,
+	'b#/2': 36,
+	'cb/3': 35,
+	'c/3': 36,
+	'c#/3': 37,
+	'db/3': 37,
+	'd/3': 38,
+	'd#/3': 39,
+	'eb/3': 39,
+	'e/3': 40,
+	'e#/3': 41,
+	'fb/3': 40,
+	'f/3': 41,
+	'f#/3': 42,
+	'gb/3': 42,
+	'g/3': 43,
+	'g#/3': 44,
+	'ab/3': 44,
+	'a/3': 45,
+	'a#/3': 46,
+	'bb/3': 46,
+	'b/3': 47,
+	'b#/3': 48,
+	'cb/4': 47,
+	'c/4': 48,
+	'c#/4': 49,
+	'db/4': 49,
+	'd/4': 50,
+	'd#/4': 51,
+	'eb/4': 51,
+	'e/4': 52,
+	'e#/4': 53,
+	'fb/4': 52,
+	'f/4': 53,
+	'f#/4': 54,
+	'gb/4': 54,
+	'g/4': 55,
+	'g#/4': 56,
+	'ab/4': 56,
+	'a/4': 57,
+	'a#/4': 58,
+	'bb/4': 58,
+	'b/4': 59,
+	'b#/4': 60,
+	'cb/5': 59,
+	'c/5': 60,
+	'c#/5': 61,
+	'db/5': 61,
+	'd/5': 62,
+	'd#/5': 63,
+	'eb/5': 63,
+	'e/5': 64,
+	'e#/5': 65,
+	'fb/5': 64,
+	'f/5': 65,
+	'f#/5': 66,
+	'gb/5': 66,
+	'g/5': 67,
+	'g#/5': 68,
+	'ab/5': 68,
+	'a/5': 69,
+	'a#/5': 70,
+	'bb/5': 70,
+	'b/5': 71,
+	'b#/5': 72,
+	'cb/6': 71,
+	'c/6': 72,
+	'c#/6': 73,
+	'db/6': 73,
+	'd/6': 74,
+	'd#/6': 75,
+	'eb/6': 75,
+	'e/6': 76,
+	'e#/6': 77,
+	'fb/6': 76,
+	'f/6': 77,
+	'f#/6': 78,
+	'gb/6': 78,
+	'g/6': 79,
+	'g#/6': 80,
+	'ab/6': 80,
+	'a/6': 81,
+	'a#/6': 82,
+	'bb/6': 82,
+	'b/6': 83,
+	'b#/6': 84,
+	'cb/7': 83,
+	'c/7': 84,
+	'c#/7': 85,
+	'db/7': 85,
+	'd/7': 86,
+	'd#/7': 87,
+	'eb/7': 87,
+	'e/7': 88,
+	'e#/7': 89,
+	'fb/7': 88,
+	'f/7': 89,
+	'f#/7': 90,
+	'gb/7': 90,
+	'g/7': 91,
+	'g#/7': 92,
+	'ab/7': 92,
+	'a/7': 93,
+	'a#/7': 94,
+	'bb/7': 94,
+	'b/7': 95,
+	'b#/7': 96,
+	'cb/8': 95,
+	'c/8': 96,
+	'c#/8': 97,
+	'db/8': 97,
+	'd/8': 98,
+	'd#/8': 99,
+	'eb/8': 99,
+	'e/8': 100,
+	'e#/8': 101,
+	'fb/8': 100,
+	'f/8': 101,
+	'f#/8': 102,
+	'gb/8': 102,
+	'g/8': 103,
+	'g#/8': 104,
+	'ab/8': 104,
+	'a/8': 105,
+	'a#/8': 106,
+	'bb/8': 106,
+	'b/8': 107,
+	'b#/8': 108,
+	'cb/9': 107,
+	'c/9': 108,
+	'c#/9': 109,
+	'db/9': 109,
+	'd/9': 110,
+	'd#/9': 111,
+	'eb/9': 111,
+	'e/9': 112,
+	'e#/9': 113,
+	'fb/9': 112,
+	'f/9': 113,
+	'f#/9': 114,
+	'gb/9': 114,
+	'g/9': 115,
+	'g#/9': 116,
+	'ab/9': 116,
+	'a/9': 117,
+	'a#/9': 118,
+	'bb/9': 118,
+	'b/9': 119,
+	'b#/9': 120,
+	'cb/0': 119,
+	'c/0': 120,
+	'c#/0': 121,
+	'db/0': 121,
+	'd/0': 122,
+	'd#/0': 123,
+	'eb/0': 123,
+	'e/0': 124,
+	'e#/0': 125,
+	'fb/0': 124,
+	'f/0': 125,
+	'f#/0': 126,
+	'gb/0': 126,
+	'g/0': 127
+    };
+
+    //maps vextab key signatures to accidentals using associative 2D array
+    var vtk = {
+	'C': {'b':'', 'e':'', 'a':'', 'd':'', 'g':'', 'c':'', 'f':''},
+	'F': {'b':'b', 'e':'', 'a':'', 'd':'', 'g':'', 'c':'', 'f':''},
+	'Bb': {'b':'b', 'e':'b', 'a':'', 'd':'', 'g':'', 'c':'', 'f':''},
+	'Eb': {'b':'b', 'e':'b', 'a':'b', 'd':'', 'g':'', 'c':'', 'f':''},
+	'Ab': {'b':'b', 'e':'b', 'a':'b', 'd':'b', 'g':'', 'c':'', 'f':''},
+	'Db': {'b':'b', 'e':'b', 'a':'b', 'd':'b', 'g':'b', 'c':'', 'f':''},
+	'Gb': {'b':'b', 'e':'b', 'a':'b', 'd':'b', 'g':'b', 'c':'b', 'f':''},
+	'Cb': {'b':'b', 'e':'b', 'a':'b', 'd':'b', 'g':'b', 'c':'b', 'f':'b'},
+	'G': {'f':'#', 'c':'', 'g':'', 'd':'', 'a':'', 'e':'', 'b':''},
+	'D': {'f':'#', 'c':'#', 'g':'', 'd':'', 'a':'', 'e':'', 'b':''},
+	'A': {'f':'#', 'c':'#', 'g':'#', 'd':'', 'a':'', 'e':'', 'b':''},
+	'E': {'f':'#', 'c':'#', 'g':'#', 'd':'#', 'a':'', 'e':'', 'b':''},
+	'B': {'f':'#', 'c':'#', 'g':'#', 'd':'#', 'a':'#', 'e':'', 'b':''},
+	'F#': {'f':'#', 'c':'#', 'g':'#', 'd':'#', 'a':'#', 'e':'#', 'b':''},
+	'C#': {'f':'#', 'c':'#', 'g':'#', 'd':'#', 'a':'#', 'e':'#', 'b':'#'},
+    };
 
     var highlightNote = function(note) {
         new VF.TickContext().addTickable(note).preFormat();
@@ -207,23 +280,30 @@ var app = function() {
     //helper function to look up midi number in table
     //returns array of midi numbers corresponding to each
     //key in a note's key array
-    var find_midi_number = function(note){
+    var find_midi_number = function(note, accidentals){
         var num = 0;
         var num_array = [];
         for(var i=0; i<note.keys.length; i++){
-            num = vtm[note.keys[i]];
+	    // insert the accidental into the name, since VexFlow itself
+	    // doesn't support that
+	    var noteName = note.keys[i];
+	    if (accidentals[i] != '' && accidentals[i] != 'n') {
+		var noteNameArray = noteName.split('/');
+		noteNameArray[0] += accidentals[i];
+		noteName = noteNameArray[0] + '/' + noteNameArray[1];
+	    }
+            num = vtm[noteName];
             num_array[i] = num;
             console.log(num);
         }
         return num_array;
-
     };
 
-    var playNote = function(note,duration) {
+    var playNote = function(note, duration, accidentals) {
         if (duration <= 0) return "duration is " + duration;
         highlightNote(note);
         //var midi = MIDI.player;
-        var midi_array = find_midi_number(note);
+        var midi_array = find_midi_number(note, accidentals);
         console.log(midi_array);
        // for( i = 0; i<midi_array.length; i++){
         MIDI.loadPlugin({
@@ -233,11 +313,11 @@ var app = function() {
 			    console.log(state, progress);
 		    },
 		    onsuccess: function() {
-			    var delay = 1; // play one note every quarter second
+			    var delay = duration; // in quarter-seconds
 			    var note = midi_array; // the MIDI note
 			    var velocity = 127; // how hard the note hits
 			    // play the note
-			    MIDI.setVolume(0, 127,0.75);
+			    MIDI.setVolume(0, 127, 0.75);
 			    MIDI.chordOn(0, note, velocity, delay);
 			    MIDI.chordOff(0, note, delay + 1);
 		    }
@@ -277,41 +357,60 @@ var app = function() {
                 return q;
                 break;
             case "8":
-                return q / 2;
+                return q / 2.0;
                 break;
             case "16":
-                return q / 4;
+                return q / 4.0;
                 break;
             case "32":
-                return q / 8;
+                return q / 8.0;
                 break;
             case "64":
-                return q / 16;
+                return q / 16.0;
+                break;
+            case "128":
+                return q / 32.0;
                 break;
             default: break;
         }
         return 0;
     };
 
-    //function to cause delay before executing code
-    function delay(ms) { var start_time = Date.now(); while (Date.now() - start_time < ms); }
+    // helper function to apply the key's accidentals
+    var calculateKey = function(note, accidentals, key) {
+	if (key == 'C') return accidentals; // the key of C adds no accidentals
+	for (var i = 0; i < accidentals.length; ++i) {
+	    if (accidentals[i] != '') continue; // manual accidentals override the key
+	    accidentals[i] = vtk[key][note.keys[i].split('/')[0]];
+	}
+	return accidentals;
+    };
 
-    var playStaff = function(notes, tempo /*optional*/, ties /*optional*/) {
+    //function to cause delay before executing code
+    //function delay(ms) { var start_time = Date.now(); while (Date.now() - start_time < ms); }
+
+    var playStaff = function(notes, noteAccidentals /*optional*/, key /*optional*/, tempo /*optional*/, ties /*optional*/) {
         // DO NOT pass in a normal ties list as an argument. Make an
         // array, where each index i's value j is what note i is tied
         // to, but j is not i. So, if i and j are tied together, then
         // ties[i] = j, and ties[j] and all others should = -1. Or, if
         // i is tied to j is tied to k, then ties[i] = j, ties[j] = k,
         // and ties[k] = -1 (and all others = -1).
-        // set default tempo if it wasn't passed as an argument:
+        // set default arguments:
         tempo = typeof tempo !== 'undefined' ? tempo : 120;
-        // duration of a quarter note in miliseconds:
-        var q = 120 / (tempo * 2) * 1000;
+        key = typeof key !== 'undefined' ? key : 'C';
+        // duration of a quarter note in quarter-seconds:
+        var q = 120 / (tempo * 2) * 4;
         for (var i = 0; i < notes.length; i++) {
             var note = notes[i];
             if (typeof ties === 'undefined' || ties[i] < 0) {
-                playNote (note, calculateDuration (note.duration, q));
-                //delay(2000);
+		if (typeof noteAccidentals !== 'undefined') {
+		    playNote (note, calculateDuration (note.duration, q), calculateKey(note, noteAccidentals[i], key));
+		} else {
+		    var accidentals = [];
+		    for (var j = 0; j < notes[i].keys.length; ++j) accidentals[j] = '';
+		    playNote (note, calculateDuration (note.duration, q), calculateKey(note, accidentals, key));
+		}
             } else { /* tied notes have not been implemented yet because
                         it would have been pretty much as massive a job
                         as all of the code made for this website all
@@ -335,9 +434,81 @@ var app = function() {
             }
         }
     };
-    playStaff(notes);
 
-/*  examples/tests are here
+    // Test staff
+    // Create an SVG renderer and attach it to the DIV element named "boo".
+    var div = document.getElementById("boo");
+    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+    // Configure the rendering context.
+    renderer.resize(500, 500);
+    var context = renderer.getContext();
+    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+    // Create a stave of width 400 at position 10, 40 on the canvas.
+    var stave = new VF.Stave(10, 40, 400);
+
+    // Add a clef and time signature.
+    stave.addClef("treble").addTimeSignature("4/4");
+
+    // Helper functions for accidentals
+    function makeNewAccid(factory) {
+	return function(accidType) {
+	  return new factory.Accidental(accidType);
+	};
+    }
+    var newAccid = makeNewAccid(VF);
+
+    // Add notes
+    var notes = [
+        // A quarter-note C.
+        new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+
+        // A quarter-note D.
+        new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
+
+        // A quarter-note rest. Note that the key (b/4) specifies the vertical
+        // position of the rest.
+        new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
+
+        // A C-Major chord.
+        new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
+	    .addAccidental(0, newAccid('n'))
+	    .addAccidental(1, newAccid('b'))
+	    .addAccidental(2, newAccid('#'))
+    ];
+
+    // We have to manually keep list of accidentals, because the way
+    // VexFlow stores them is a garbled mess (just an unhelpful array
+    // of numbers).
+    // We'll also have to manually keep track of the fact that if an
+    // accidental is rendered once, it applies to every subsequent time
+    // that note appears in that measure. For example, if you have
+    // |----|Ab--A|A---|, then that 2nd A will also get a 'b' in
+    // noteAccidentals, but the 3rd A will not, and both 2nd and 3rd As
+    // will *not* get an .addAccidental('b').
+    var noteAccidentals = [];
+    for (var i = 0; i < notes.length; ++i) {
+	noteAccidentals[i] = [];
+	for (var j = 0; j < notes[i].keys.length; ++j)
+	    noteAccidentals[i][j] = '';
+    }
+    noteAccidentals[3] = ['n', 'b', '#'];
+
+    var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    voice.addTickables(notes);
+
+    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
+
+    voice.draw(context, stave);
+
+    // examples/tests are here
+    playStaff(notes, noteAccidentals);
+
+/*
     for (var i = 0; i < notes.length; ++i) {
         var note = notes[i];
         if (i == 0 || i == 2) {
--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$bf1359bc98f189f1$9b07c4a98b2a1acea541d955fdfc87dda7fd5694"
+password="pbkdf2(1000,20,sha512)$a61c2940531881de$ce014db545fe1d82f83d8d827472fff6bcc64f2e"
